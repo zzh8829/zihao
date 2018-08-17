0d09b7effec568006428103d541a35fd9ac20d9f
Zihao Zhang <zzh8829@gmail.com>
2018-08-17 15:06:21 -0700

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM nginx:alpine
-COPY  deploy/nginx_gcs_proxy.conf /etc/nginx/
+COPY deploy/nginx_gcs_proxy.conf /etc/nginx/
 COPY deploy/nginx_zihao.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 8000
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,10 +6,8 @@ const browserSync = BrowserSync.create();
 const webpackOptions = ["--colors", "--display-error-details"];
 const hugoOptions = ["-v"];
 
-gulp.task("hugo", done => runHugo(done));
-gulp.task("webpack", done =>
-  runWebpack(["--mode production", "--profile"], done)
-);
+gulp.task("webpack", () => runWebpack(["--mode", "production", "--profile"]));
+gulp.task("hugo", () => runHugo());
 
 gulp.task("build", gulp.parallel("webpack", "hugo"));
 
@@ -25,28 +23,14 @@ gulp.task("watch", () => {
   gulp.watch("./public/**/*").on("change", browserSync.reload);
 });
 
-function runHugo(options, done = null) {
-  return cp
-    .spawn("hugo", hugoOptions.concat(options || []), { stdio: "inherit" })
-    .on("close", code => {
-      if (code === 0) {
-        done && done()
-      } else {
-        done && done("Hugo failed! :(");
-      }
-    });
+function runHugo(options) {
+  return cp.spawn("hugo", hugoOptions.concat(options || []), {
+    stdio: "inherit"
+  });
 }
 
-function runWebpack(options, done = null) {
-  return cp
-    .spawn("webpack", webpackOptions.concat(options || []), {
-      stdio: "inherit"
-    })
-    .on("close", code => {
-      if (code === 0) {
-        done && done()
-      } else {
-        done && done("Webpack failed! :(");
-      }
-    });
+function runWebpack(options) {
+  return cp.spawn("webpack", webpackOptions.concat(options || []), {
+    stdio: "inherit"
+  });
 }
