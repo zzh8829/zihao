b0cedaff78c7d311d48d4e89af19d74d561475ab
Zihao Zhang <zzh8829@gmail.com>
2019-08-29 13:44:42 -0400

    No more hard code

--- static/dotfiles.sh
+++ static/dotfiles.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-set -ex
-
-cd ~
-rm -rf ~/.dotfiles
-git clone git@github.com:zzh8829/dotfiles .dotfiles
-cd .dotfiles
-./dotfiles.sh
+curl https://raw.githubusercontent.com/zzh8829/dotfiles/master/install.sh | sh
