9877cd15be020e3a33b853235a29fcdf62b7055a
Zihao Zhang <zzh8829@gmail.com>
2019-10-13 15:55:26 -0700

    Add dark theme

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,22 @@
-import * as Sentry from "@sentry/browser";
+import * as Sentry from '@sentry/browser';
+import Darkmode from 'darkmode-js';
 
-console.log("ENV", process.env.NODE_ENV);
+console.log('ENV', process.env.NODE_ENV);
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV == 'production') {
   Sentry.init({
-    dsn: "https://e7cdad7d71814f71b317fe387e00b938@sentry.io/1757753"
+    dsn: 'https://e7cdad7d71814f71b317fe387e00b938@sentry.io/1757753',
   });
 }
 
 $(() => {
-  const pathname = window.location.pathname.replace(/\/+$/, "");
-  $('nav > a[href="' + pathname + '"]').addClass("active");
+  const pathname = window.location.pathname.replace(/\/+$/, '');
+  $('nav > a[href="' + pathname + '"]').addClass('active');
 });
+
+var options = {
+  label: '🌓', // default: ''
+  saveInCookies: true,
+};
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
